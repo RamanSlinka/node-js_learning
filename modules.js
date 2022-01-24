78614ac1d09fa34405e542aaa874f54cce7b03a0
RamanSlinka <romuch1986@gmail.com>
2022-01-24 22:18:14 +0100

    Node.js & File System

--- modules.js
+++
@@ -1,9 +0,0 @@
-const os = require('os')
-const {userName: user, sayHi} = require('./test');
-
-const name = 'Robert'
-
-console.log(sayHi(name));
-console.log(os.platform(), os.release());
-
-module.exports = name;
--- test.js
+++
@@ -1,8 +0,0 @@
-const userName = 'Fama';
-const sayHi = (userName) => `hello, my name is ${userName}`;
-//console.log(sayHi(userName))
-
-module.exports ={
-    userName,
-    sayHi,
-}
