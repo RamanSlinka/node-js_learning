b1196b9eaad0a3f12e44caa67a4a0ba4f27e6980
RamanSlinka <romuch1986@gmail.com>
2022-01-25 23:15:47 +0100

    Create Server( - my first server ;) )

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const fs = require('fs');
-const zlip = require('zlib');
+const http = require('http');
 
-const readStream = fs.createReadStream('./docs/text.txt');
+const PORT = 3000;
 
-const writeStream = fs.createWriteStream('./docs/new-text.txt');
+const server = http.createServer((req, res) => {
+    console.log("Server request");
+    console.log(req.url, req.method);
 
-const compressStream = zlip.createGzip();
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<head><link rel="stylesheet" href="#"></head>')
+    // res.write('<h1>Hello World!</h1>')
+    // res.write('<p>My name is Raman </p>')
 
-// readStream.on('data', (chunk) => {
-//     writeStream.write('-----CHUNK START-------');
-//     writeStream.write(chunk);
-//     writeStream.write('-----CHUNK END-------')
-// })
+    res.setHeader('Content-Type', 'application/json')
 
-const handleError = () => {
-    console.log("Error");
-    readStream.destroy();
-    writeStream.end('Finished wish error...')
-}
+    const data = JSON.stringify([
+        { name: "Adam", age: 30},
+        { name: "Artur", age: 32},
+        { name: "Rafal", age: 45}
+    ])
 
-readStream
-    .on('error', handleError)
-    .pipe(compressStream)
-    .pipe(writeStream)
-    .on('error', handleError);
+
+    res.end(data);
+})
+
+server.listen(PORT, 'localhost', (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`  )
+})
