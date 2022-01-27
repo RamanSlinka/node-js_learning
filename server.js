2a9e60d1adb5962d759c4fa1a1613d4b7f07e41e
RamanSlinka <romuch1986@gmail.com>
2022-01-27 22:49:18 +0100

    change in layout and added new files (post, posts, add-post)

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path')
 
-const app =express();
+const app = express();
 
 const PORT = 3000;
 
 const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
 
 
-app.listen(PORT,  (error) => {
-    error ? console.log(error) : console.log(`listening port ${PORT}` );
+app.listen(PORT, (error) => {
+    error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
 app.get('/', (req, res) => {
@@ -19,12 +19,22 @@ app.get('/', (req, res) => {
 app.get('/contacts', (req, res) => {
     res.sendFile(createPath('contacts'))
 });
+app.get('/posts/:id', (req, res) => {
+    res.sendFile(createPath('post'))
+});
+app.get('/posts', (req, res) => {
+    res.sendFile(createPath('posts'))
+});
+app.get('/add-post', (req, res) => {
+    res.sendFile(createPath('add-post'))
+});
+
 
 app.get('/about-us', (req, res) => {
     res.redirect('/contacts')
 });
 
-app.use( (req, res) => {
+app.use((req, res) => {
     res.status(404)
     res.sendFile(createPath('error'))
 });
