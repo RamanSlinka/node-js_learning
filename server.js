03d48700bfb66148ff3decf8e559d4d766812b32
RamanSlinka <romuch1986@gmail.com>
2022-01-28 22:34:38 +0100

    change in layout and added new files (post, posts, add-post)

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path')
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) => path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 
 app.listen(PORT, (error) => {
@@ -13,11 +15,14 @@ app.listen(PORT, (error) => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(createPath('index'))
+    res.render(createPath('index'))
 });
 
 app.get('/contacts', (req, res) => {
-    res.sendFile(createPath('contacts'))
+    res.render(createPath('contacts'))
+});
+app.get('/posts/:id', (req, res) => {
+    res.render(createPath('post'))
 });
 app.get('/posts/:id', (req, res) => {
     res.sendFile(createPath('post'))
@@ -28,6 +33,13 @@ app.get('/posts', (req, res) => {
 app.get('/add-post', (req, res) => {
     res.sendFile(createPath('add-post'))
 });
+app.get('/posts', (req, res) => {
+    res.render(createPath('posts'))
+});
+app.get('/add-post', (req, res) => {
+    res.render(createPath('add-post'))
+});
+
 
 
 app.get('/about-us', (req, res) => {
@@ -36,6 +48,6 @@ app.get('/about-us', (req, res) => {
 
 app.use((req, res) => {
     res.status(404)
-    res.sendFile(createPath('error'))
+    res.render(createPath('error'))
 });
 
