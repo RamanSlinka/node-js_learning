03be10ad5dc519396d2fbceee1e2f6ff6e10f565
RamanSlinka <romuch1986@gmail.com>
2022-02-03 22:27:35 +0100

    Much more changed (more refactored); "MVC Pattern"

--- models/post.js
+++ models/post.js
@@ -2,21 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    text: {
-        type: String,
-        required: true,
-    },
-    title: {
-        type: String,
-        required: true,
-    },
-    author: {
-        type: String,
-        required: true,
-    },
-
-}, {timestamps: true});
+  text: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema);
-
 module.exports = Post;
