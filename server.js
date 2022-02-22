5d424ed56e07107fb3507101b1499157087ee15d
RamanSlinka <romuch1986@gmail.com>
2022-02-22 22:25:49 +0100

    added dotenv & chalk; deploy

--- server.js
+++ server.js
@@ -1,26 +1,31 @@
 const express = require('express');
+//const chalk = require('chalk');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const methodOverride = require('method-override');
 const postRoutes = require('./routes/post-routes');
 const contactRoutes = require('./routes/contact-routes');
 const createPath = require('./helpers/create-path');
 const postApiRouting = require('./routes/api-post-routes');
 
+
+// const errorMsg = chalk.bgKeyword('white').redBright;
+// const succesMsg = chalk.bgKeyword('green').white;
+
 const app = express();
 
 app.set('view engine', 'ejs');
 
-const PORT = 3000;
-const db = 'mongodb+srv://Roman:5220780@cluster.2imnv.mongodb.net/node-blog?retryWrites=true&w=majority';
+
 
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((res) => console.log('Connected to DB'))
   .catch((error) => console.log(error));
 
-app.listen(PORT, (error) => {
-  error ? console.log(error) : console.log(`listening port ${PORT}`);
+app.listen(process.env.PORT, (error) => {
+  error ? console.log(error) : console.log(`listening port ${process.env.PORT}`);
 });
 
 app.use(express.urlencoded({ extended: false }));
